1c8eb187a733b6931e93b13fd1395e21758666aa
shankaarunoda <shankaarunoda96@gmail.com>
2023-09-20 12:58:09 +0530

    [Add] Innitialy added tdm_document_templates

---
+++ tdm_document_templates/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Financial Document Templates',
+    'version': '1.1',
+    'license': 'LGPL-3',
+    'category': '',
+    'sequence': 2,
+    'author': "Sidath Liyanage",
+    'website': '',
+    'summary': 'Financial document templates for TDM',
+    'description': """Financial document templates for TDM""",
+    'depends': ['base', 'account'],
+    'data': [
+        'data/report_data.xml',
+        'views/report_invoice.xml',
+        'views/report_order_confirmation.xml',
+        # 'views/report_vendor_bill.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
